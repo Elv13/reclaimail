7e0fa62bf1c7d3b6972d30e6030938210af35006
Emmanuel Lepage Vallee <elv1313@gmail.com>
2019-05-15 17:16:27 +0000

    notmuch: Use the Lua code by default.

--- docker-notmuch/run.sh
+++ docker-notmuch/run.sh
@@ -1,37 +1,17 @@
 #! /bin/sh
 
-#sed -i "s/YOUR_EMAIL/$EMAIL/" /home/offlineimap/.offlineimaprc
-#sed -i "s/YOUR_CLIENT_ID/$CLIENT_ID/" /home/offlineimap/.offlineimaprc
-#sed -i "s/YOUR_ACCESS_TOKEN/$ACCESS_TOKEN/" /home/offlineimap/.offlineimaprc
-#sed -i "s=YOUR_SECRET=$SECRET=" /home/offlineimap/.offlineimaprc
+LUA_PATH=$(luajit -e "print(package.path)")
+LUA_CPATH=$(luajit -e "print(package.cpath)")
 
-#if [ ! -e '~/.offlineimap' ]; then
-#    mkdir -p /home/offlineimap/GMail/.offlineimap
-#    ln -s /home/offlineimap/GMail/.offlineimap ~
-#fi
+LUA_PATH="$LUA_PATH;$HOME/LPeg-Parsers-master/?.lua"
+LUA_CPATH="$LUA_CPATH;$HOME/LPeg-Parsers-master/?.so"
+LUA_PATH="$LUA_PATH;$HOME/notmuchlua/?.lua"
 
-#mkdir -p /home/offlineimap/GMail/Mutt
-#ln -s /home/offlineimap/GMail/Mutt ~/
+export LUA_PATH=$LUA_PATH
+export LUA_CPATH=$LUA_CPATH
 
-notmuch new
+# Add (or update) the maildir hooks
+mkdir -p ~/GMail/.notmuch/hooks
+cp hooks/* ~/GMail/.notmuch/hooks
 
-echo "Adding KDE tags"
-notmuch tag +KDE -- 'from:*@kde.org'
-notmuch tag +KDE -- 'cc:*@kde.org'
-
-echo "Adding GitHub tags"
-notmuch tag +GitHub -- 'from:*github.com'
-notmuch tag +GitHub -- 'from:*@github.com'
-notmuch tag +GitHub -- 'from:notifications@github.com'
-
-echo "Adding Awesome tags"
-notmuch tag +Awesome -- 'subject:[awesomeWM/awesome]*'
-notmuch tag +Awesome -- 'from:awesome@noreply.github.com' -- 'to:awesome@noreply.github.com'
-notmuch tag +Awesome -- 'from:awesome@noreply.github.com'
-notmuch tag +Awesome -- 'to:awesome@noreply.github.com'
-notmuch tag +Awesome -- 'to:awesomeWM/awesome'
-notmuch tag +Awesome -- 'from:@naquadah.org'
-notmuch tag +Awesome -- 'from:*@naquadah.org'
-
-echo "Adding Voicemail tags"
-notmuch tag +Voicemail -- 'to:elv1313+voicemail@gmail.com'
+luajit $HOME/notmuchlua/process.lua
